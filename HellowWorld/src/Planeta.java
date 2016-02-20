6e20d35b1811b7eb37689b9f11cfa3f96def37fc
basp91 <basp91@gmail.com>
2016-02-20 14:44:35 -0600

    Sesión 2

---
+++ HellowWorld/src/Planeta.java
@@ -0,0 +1,29 @@
+
+public enum Planeta {
+	MERCURIO(3.303+23, 2.4397e6),
+	TIERRA(5.976e+24, 6.37814e6),
+	MARTE(6.421e+23,3.3972e6),
+	JUPITER(1.9e+27,7.1492e7);
+	
+	private double masa;
+	private double radio;
+	
+	Planeta(double masa, double radio){
+		this.masa = masa;
+		this.radio = radio;
+	}
+
+	public double getMasa() {
+		return masa;
+	}
+
+	public double getRadio() {
+		return radio;
+	}
+	
+	public double dobleMasa(){
+		return masa*2;
+	}
+	
+
+}
---
+++ HellowWorld/src/Punto.java
@@ -0,0 +1,42 @@
+
+public class Punto {
+	
+	private double x;
+	private double y;
+	
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "Punto [x=" + x + ", y=" + y + "]";
+	}
+	
+	public double distancia(Punto punto){
+		System.out.println("Calculando la distancia");
+		System.out.println(punto.toString());
+		System.out.println(this.toString());
+		
+		return Math.sqrt(Math.pow(x-punto.getX(), 2)
+				+Math.pow(y-punto.getY(),2));
+	}
+	public void desplazar(double dx, double dy){
+		x+=dx;
+		y+=dy;
+	}
+	
+}
